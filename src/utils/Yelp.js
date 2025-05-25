fd44f2e1fd2043d582e04126ea95a22424566b58
Alif Fachrel Anargya <aliffachrel@gmail.com>
2025-05-25 20:49:39 +0700

    Add Yelp API utility with search function for business queries

--- src/utils/Yelp.js
+++ src/utils/Yelp.js
@@ -1,2 +1,9 @@
 export const apiKey = process.env.REACT_APP_YELP_API_KEY;
 
+const Yelp = {
+  search(searchTerm, location, sortOption) {
+    const url = `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}&sort_by=${sortOption}`;
+
+    return fetch(url, {});
+  },
+};
